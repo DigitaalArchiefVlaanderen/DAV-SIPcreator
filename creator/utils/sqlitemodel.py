6cb764e95692f403272355574f7cbe5a83db6714
ThijsIT <thijsit@hotmail.com>
2024-07-01 02:11:25 +0200

    Add basic error checking

--- creator/utils/sqlitemodel.py
+++ creator/utils/sqlitemodel.py
@@ -1,8 +1,21 @@
-from PySide6 import QtCore
+from enum import Enum
+
+from PySide6 import QtCore, QtGui
 
 import sqlite3 as sql
 
+
+class Color(Enum):
+    RED = QtGui.QBrush(QtGui.QColor(255, 0, 0))
+    YELLOW = QtGui.QBrush(QtGui.QColor(255, 255, 0))
+    GREY = QtGui.QBrush(QtGui.QColor(230, 230, 230))
+
+
 class SQLliteModel(QtCore.QAbstractTableModel):
+    bad_rows_changed: QtCore.Signal = QtCore.Signal(
+        *(int,), arguments=["bad_rows_left"]
+    )
+
     def __init__(
         self,
         table_name: str,
@@ -27,6 +40,9 @@ class SQLliteModel(QtCore.QAbstractTableModel):
         self.row_count, self.col_count = -1, -1
 
         self._data: list[list[str]] = []
+        self.colors: dict[tuple[int, int], Color] = {}
+        self.tooltips: dict[tuple[int, int], str] = {}
+
         self.get_data()
 
         # self.calculate_shape()
@@ -47,7 +63,7 @@ class SQLliteModel(QtCore.QAbstractTableModel):
         row, col = index.row(), index.column()
 
         # NOTE: quotes are not allowed for now
-        self._data[row][col] = new_value.replace('"', "").replace("'", "")
+        self._data[row][col] = str(new_value).replace('"', "").replace("'", "")
 
     def calculate_shape(self):
         with self.conn as conn:
@@ -101,6 +117,11 @@ class SQLliteModel(QtCore.QAbstractTableModel):
 
             self.col_count = len(self.columns)
 
+        # NOTE: for the checks, set all the cells
+        for row_index, row in enumerate(self._data):
+            for col_index, value in enumerate(row):
+                self.setData(self.index(row_index, col_index), value)
+
         # NOTE: not very good to use this method for retrieval and getting
         return self._data
 
@@ -128,18 +149,36 @@ class SQLliteModel(QtCore.QAbstractTableModel):
         if not index.isValid():
             return
 
+        row, col = index.row(), index.column()
+        _id = self._data[row][0]
+
         if (
             role == QtCore.Qt.ItemDataRole.DisplayRole
             or role == QtCore.Qt.ItemDataRole.EditRole
         ):
             return self.get_value(index)
+        elif role == QtCore.Qt.ItemDataRole.BackgroundRole:
+            color = self.colors.get((_id, col))
+
+            if color:
+                return color.value
+        elif role == QtCore.Qt.ItemDataRole.ToolTipRole:
+            tooltip = self.tooltips.get((_id, col))
+
+            if tooltip:
+                return tooltip
 
     def setData(self, index, value, role=QtCore.Qt.ItemDataRole.EditRole):
         if not index.isValid():
             return False
 
+        row, col = index.row(), index.column()
+
         if role == QtCore.Qt.ItemDataRole.EditRole:
             self.set_value(index, value)
+
+            if self.columns[col] == "Path in SIP":
+                self.path_in_sip_check(row, col, value)
 
             return True
 
@@ -173,3 +212,30 @@ class SQLliteModel(QtCore.QAbstractTableModel):
         )
 
         self.layoutChanged.emit()
+
+    def _mark_cell(self, row: int, col: int, color: Color = None, tooltip: str = None) -> None:        
+        _id = self._data[row][0]
+
+        if not color:
+            # Unmark
+            if (_id, col) in self.colors:
+                del self.colors[(_id, col)]
+
+            if (_id, col) in self.tooltips:
+                del self.tooltips[(_id, col)]
+        else:
+            self.colors.setdefault((_id, col), color)
+
+            if tooltip:
+                self.tooltips.setdefault((_id, col), tooltip)
+
+        self.bad_rows_changed.emit(len(self.colors))
+
+    # NOTE: Checks
+    def path_in_sip_check(self, row: int, col: int, value: str) -> None:
+        if value == "":
+            self._mark_cell(row, col, Color.RED, "Path in SIP mag niet leeg zijn")
+        elif "/" in value:
+            self._mark_cell(row, col, Color.RED, "Path in SIP mag geen '/' bevatten")
+        else:
+            self._mark_cell(row, col)
